820374ee28d8cbec17eb3c88c622f6e84d4d9b4a
Sean Doyle <sdoylelambda@gmail.com>
2019-01-24 15:44:45 -0500

    Storyboard with collection VC outlets added.

---
+++ ios photos 1.24/ios photos 1.24/PhotosCollectionViewCell.swift
@@ -0,0 +1,9 @@
+
+import UIKit
+
+class PhotosCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageViewOutlet: UIImageView!
+    @IBOutlet weak var TextCellLabel: UILabel!
+    
+}
